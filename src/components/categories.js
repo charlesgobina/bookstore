cc0e688d4c723b619fb5f4adedc6ce4571ad01a2
charlesgobina <charles.gobina@gmail.com>
2022-01-20 09:20:30 +0100

    Categories component

---
+++ src/components/categories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Categories() {
+  return (
+    <div className="cat-list">
+      <p>Under Construction</p>
+    </div>
+  );
+}
+
+export default Categories;
