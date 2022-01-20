a5591fbdd2da9be80d23907c47bd1e66a61080c1
charlesgobina <charles.gobina@gmail.com>
2022-01-20 09:23:34 +0100

    Root component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Books from './redux/books/books';
-import Categories from './redux/categories/categories';
+import Books from './components/bookContainer';
+import Categories from './components/categories';
 import Header from './components/Header';
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <Header />
-    <Routes>
-      <Route path="/" element={<Books />} />
-      <Route path="/categories" element={<Categories />} />
-    </Routes>
-  </div>
-);
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </div>
+  );
+}
 
 export default App;
